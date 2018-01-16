8fd6d6c4bdd36ef9fd5fe368ca62bc06e70beb7c
Suzannah James <suzannahsmjames1@gmail.com>
2018-01-16 20:05:19 +0000

    initial setup

---
+++ prototype/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+
+gulp.task('default', function(done) {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
+  
+  gulp.watch("./sass/main.scss", ["sass"]);
+  
+  done();
+})
+
+
+gulp.task("sass", function() {
+  return gulp.src("./sass/main.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("./assets/css"))
+    .pipe(browserSync.stream());
+});
+
+
+
