2ee8c8380041a6c9dae0701cbc39702e6022282e
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2018-01-24 17:08:47 +0000

    What We Do specific (tabs) (#14)
    * add beginnings of header and footer
    
    * set up colours
    
    * begin about
    
    * basic tabs
    
    * finished tab system
    
    * add titles
    
    * remove tab from index
    
    * added data to tabs
    
    * fix initial loading bug
    
    * finish tabs
    
    * add fallback
    
    * update accessible tabs
    
    * add defaults
    
    * cleanup

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,5 +1,48 @@
 (function () {
+  'use strict';
   var d = document;
+  document.documentElement.className = document.documentElement.className.replace('no-js', 'js');
+
+  // Helper Classes
+  var _ = function (node) {
+
+      if(!node) {
+        throw Error('Please specify element or node');
+        return;
+      }
+
+      if(node.nodeType !== Node.ELEMENT_NODE) {
+        if(d.querySelector(node)) {
+          node = d.querySelector(node);
+        } else {
+          throw Error('No element matching that description');
+        }
+      }
+
+      var hasClass = function (_class) {
+        return !!node.className.match(_class);
+      };
+
+      var addClass = function (_class) {
+        if (!hasClass(_class)) {
+          node.className += ' ' + _class;
+          node.className = node.className.trim();
+        }
+      };
+
+      var removeClass = function (_class) {
+        if (hasClass(_class)) {
+          node.className = node.className.replace(_class, '').trim();
+        }
+      };
+
+      return {
+        hasClass: hasClass,
+        removeClass: removeClass,
+        addClass: addClass
+      }
+  };
+
   var tabs = d.querySelector('.tabs');
 
   if (tabs) {
@@ -7,17 +50,32 @@
     var tabShowClass = 'tabs__link--active';
     var links = tabs.querySelectorAll('.tabs__link');
     var content = tabs.querySelectorAll('.tabs__item');
+    var banner = d.querySelector('.banner');
+    var bannerHeader = banner.querySelector('.banner__header');
     var initialCall = true;
 
     var tabEvents = {
       showActiveTab: function (args) {
-        id = args.id.replace(/^#/, '');
-        tabs.querySelector('[href="#' + id + '"]').classList.add(tabShowClass);
-        d.getElementById(id).classList.remove(tabsHideClass);
+        var id = args.id.replace(/^#/, '');
+        tabs.querySelector('[href="#' + id + '"]').setAttribute('aria-selected', true);
+        tabs.querySelector('[href="#' + id + '"]').setAttribute('title', 'Active tab');
+        d.getElementById(id).setAttribute('aria-hidden', 'false');
+
+        _(tabs.querySelector('[href="#' + id + '"]')).addClass(tabShowClass);
+        _(tabs.querySelector('#' + id)).removeClass(tabsHideClass);
+
+        if(banner && bannerHeader && args.img && args.text) {
+          banner.style.backgroundImage = "url('" + args.img + "')";
+          bannerHeader.innerText = args.text;
+        }
       },
       resetTabs: function (body, index) {
-        body.classList.add(tabsHideClass);
-        links[index].classList.remove(tabShowClass);
+        body.setAttribute('aria-hidden', 'true');
+        links[index].setAttribute('aria-selected', false);
+        links[index].removeAttribute('title');
+
+        _(body).addClass(tabsHideClass);
+        _(links[index]).removeClass(tabShowClass);
 
         if(index === content.length - 1 && initialCall) {
           tabEvents.setInitialTab();
@@ -25,20 +83,29 @@
       },
       clickHandler: function (e) {
         e.preventDefault();
+        window.location.hash = e.target.hash;
+
         [].forEach.call(content, tabEvents.resetTabs);
-        tabEvents.showActiveTab({id: e.target.hash});
+
+        tabEvents.showActiveTab({
+          id: e.target.hash,
+          img: e.target.dataset.img || null,
+          text: e.target.dataset.text || null
+        });
+
       },
       setInitialTab: function () {
-        var windowHash = window.location.hash;
-        var initialTab = content[0].id;
+        var link = tabs.querySelector('[href="' + window.location.hash + '"]');
+        var initialTab = link
+          ? { id: window.location.hash, img: link.dataset.img, text: link.dataset.text }
+          : { id: content[0].id, img: links[0].dataset.img, text: links[0].dataset.text };
 
-        if(windowHash) {
-          initialTab = tabs.querySelector(windowHash)
-            ? tabs.querySelector(windowHash).id
-            : content[0].id;
-        }
+        tabEvents.showActiveTab(initialTab);
 
-        tabEvents.showActiveTab({id: initialTab});
+        setTimeout(function() {
+          window.scrollTo(0, 0);
+        }, 0);
+
         initialCall = false;
       },
       bindClickEvent: function (link) {
