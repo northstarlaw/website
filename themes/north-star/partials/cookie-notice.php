e4ad3085f526e42de2df4c67e2b6db7114fa33b2
Samuel Gregory <fakesamgregory@users.noreply.github.com>
2018-08-23 16:35:31 +0100

    add cookie notice (#80)
    * add cookie notice
    
    * remove background
    
    * dont show cookie if post type doesnt exist

---
+++ themes/north-star/partials/cookie-notice.php
@@ -0,0 +1,21 @@
+    <?php
+        $args = array( 'post_type' => 'cookie', 'posts_per_page' => 1 );
+        $the_query = new WP_Query( $args );
+    ?>
+    <?php if ( $the_query->have_posts() ) : ?>
+        <div class="cookie-notice">
+        <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
+            <div class="container">
+                <?php the_content(); ?>
+
+                <button class="cookie-notice-close js-cookie-close" type="button">
+                    <span class="sr-only">Close Cookie</span>
+                    X
+                </button>
+            </div>
+            <?php wp_reset_postdata(); ?>
+        <?php endwhile; ?>
+        </div>
+    <?php else:  ?>
+    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    <?php endif; ?>
